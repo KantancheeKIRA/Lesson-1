7153d291785f858fbf5a631807b503419806b316
KantancheeKIRA <Kantancheekhang@gmail.com>
2022-12-07 17:14:54 -0600

    finished lesson started worksheet

---
+++ Lesson-1/src/ProblemOne.java
@@ -0,0 +1,8 @@
+public class ProblemOne {
+
+    public static void main(String[] args){
+        System.out.println("I'm better than you");
+
+        
+    }
+}
---
+++ Lesson-1/src/Variable.java
@@ -0,0 +1,30 @@
+public class Variable {
+
+    public static void main(String[] args){
+
+        // datatype notes = helpful;
+
+        int someInt = 50; // integers variable or number value
+        long somelong = 100000000; // big number
+        String someString = "Text"; // words or sentences 
+        double someDouble = 0.1; // decimal points values / anything with a dot in a number
+        char aChar = 'a'; // useless old and pathetic just one letter
+
+        // true or false commands 
+        boolean aBool = false; // or 
+        boolean anotherBool = true;
+
+        System.out.println();
+
+        // operators 
+
+        int math = 1+1; // add
+        math = 1*1;
+        math = 1/1;
+        math = 1 % 1;
+        math++;
+        math--;
+
+
+    }
+}
